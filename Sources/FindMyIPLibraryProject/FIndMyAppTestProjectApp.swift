f135c976673f069d9a43d8e818ad2cc7caac8e34
Murugesh <murugesh.sm10@gmail.com>
2024-01-11 13:52:04 +0530

    Commented main in my project for duplication main issue

--- Sources/FindMyIPLibraryProject/FIndMyAppTestProjectApp.swift
+++ Sources/FindMyIPLibraryProject/FIndMyAppTestProjectApp.swift
@@ -5,13 +5,13 @@
 //  Created by Narendran Elumalai on 11/01/24.
 //
 
-import SwiftUI
-
-@main
-struct FIndMyAppTestProjectApp: App {
-    var body: some Scene {
-        WindowGroup {
-            FindMyIPView()
-        }
-    }
-}
+//import SwiftUI
+//
+//@main
+//struct FIndMyAppTestProjectApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            FindMyIPView()
+//        }
+//    }
+//}
